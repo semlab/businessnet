f163852ff738d02f995603285325598886ed3c6a
godidier <godidier@gmail.com>
2020-11-04 02:30:40 +0900

    adding sample data test file

---
+++ test_data.py
@@ -0,0 +1,205 @@
+
+sample_reuter = """
+<!DOCTYPE lewis SYSTEM "lewis.dtd">
+<REUTERS TOPICS="YES" LEWISSPLIT="TRAIN" CGISPLIT="TRAINING-SET" OLDID="9914" NEWID="5001">
+<DATE>13-MAR-1987 15:05:02.92</DATE>
+<TOPICS><D>acq</D></TOPICS>
+<PLACES><D>usa</D></PLACES>
+<PEOPLE></PEOPLE>
+<ORGS></ORGS>
+<EXCHANGES></EXCHANGES>
+<COMPANIES></COMPANIES>
+<UNKNOWN> 
+&#5;&#5;&#5;F
+&#22;&#22;&#1;f0215&#31;reute
+d f BC-PLM-&lt;PLMA>-UNIT-ENDS   03-13 0076</UNKNOWN>
+<TEXT>&#2;
+<TITLE>PLM &lt;PLMA> UNIT ENDS MERGER TALKS</TITLE>
+<DATELINE>    SAN FRANCISCO, March 13 - </DATELINE><BODY>PLM Cos Inc said its PLM Power Co
+unit broke off merger discussions with Sunlaw Energy Corp of
+Beverly Hills, Calif.
+    In January PLM Power entered into a letter of intent to
+negotiate a potential acquisition of Sunlaw, subject to
+substantial due diligence, the company said.
+    But it also said the two companies were not able to agree
+on mutually satisfactory final terms and conditions.
+ Reuter
+&#3;</BODY></TEXT>
+</REUTERS>
+<REUTERS TOPICS="YES" LEWISSPLIT="TRAIN" CGISPLIT="TRAINING-SET" OLDID="9915" NEWID="5002">
+<DATE>13-MAR-1987 15:08:53.08</DATE>
+<TOPICS><D>coffee</D></TOPICS>
+<PLACES><D>colombia</D></PLACES>
+<PEOPLE></PEOPLE>
+<ORGS></ORGS>
+<EXCHANGES></EXCHANGES>
+<COMPANIES></COMPANIES>
+<UNKNOWN> 
+&#5;&#5;&#5;C T
+&#22;&#22;&#1;f0224&#31;reute
+b f BC-colombia-opens-coffee   03-13 0086</UNKNOWN>
+<TEXT>&#2;
+<TITLE>COLOMBIA OPENS APRIL/MAY COFFEE REGISTRATIONS</TITLE>
+<DATELINE>    BOGOTA, March 13 - </DATELINE><BODY>Colombia opened coffee export
+registrations for April and May with the National Coffee
+Growers' Federation setting no limit, Gilberto Arango,
+president of the private exporters' association, said.
+    He told Reuters the decision not to put any limit responded
+to "new factors" which have emerged from recent International
+Coffee Organisation talks in London, where producers and
+consumers failed to agree on a re-introduction of export quotas.
+ Reuter
+&#3;</BODY></TEXT>
+</REUTERS>
+<REUTERS TOPICS="YES" LEWISSPLIT="TRAIN" CGISPLIT="TRAINING-SET" OLDID="9916" NEWID="5003">
+<DATE>13-MAR-1987 15:11:52.98</DATE>
+<TOPICS><D>grain</D><D>corn</D></TOPICS>
+<PLACES><D>usa</D></PLACES>
+<PEOPLE></PEOPLE>
+<ORGS></ORGS>
+<EXCHANGES></EXCHANGES>
+<COMPANIES></COMPANIES>
+<UNKNOWN> 
+&#5;&#5;&#5;C G L
+&#22;&#22;&#1;f0228&#31;reute
+b f BC-/USDA-REPORTS-10.572   03-13 0098</UNKNOWN>
+<TEXT>&#2;
+<TITLE>USDA REPORTS 10.572 MLN ACRES IN CONSERVATION</TITLE>
+<DATELINE>    WASHINGTON, March 13 - </DATELINE><BODY>The U.S. Agriculture Department has
+accepted 10,572,402 more acres of highly erodable cropland into
+the Conservation Reserve Program, USDA announced.
+    In the latest signup, farmers on 101,020 farms submitted
+bids on a total of 11,254,837 acres.
+    The accepted bids for annual rental payments ranged up to
+90 dlrs per acre with an average of 51.17 dlrs per acre.
+    Land entered into the Conservation Reserve Program will be
+ineligible for farming for ten years and must be planted with
+permanent vegetative cover.
+    Producers enrolled 1,894,764 acres of corn base acreage in
+the conservation program to take advantage of a corn "bonus"
+rental payment that was offered by USDA.
+    The corn bonus, to be paid in generic comodity
+certificates, amounts to two dlrs per bushel, based on the ASCS
+program payment yield for corn, for each acre of corn based
+accepted into the reserve.
+    The state showing the biggest enrollment in the
+conservation program during this signup was Texas with
+approximately 1.225 mln acres, followed by Iowa with 1.030 mln
+acres, Minnesota with 890,000 acres, Montana 875,000 acres, and
+Kansas with 842,000 acres.
+    Other states showing big enrollment were Missouri with
+646,000 acres, North Dakota with 588,000 acres, and Nebraska
+with 554,000 acres.
+    In the corn belt states of Illinois and Indiana, 217,000
+acres and 116,000 acres respectively were enrolled.
+    Farm land signed up to date in the conservation program
+totals 19,488,587 acres. Bids on the previous signups ranged up
+to 90 dlrs per acre with an average of 45.52 dlrs.
+ Reuter
+&#3;</BODY></TEXT>
+</REUTERS>
+<REUTERS TOPICS="YES" LEWISSPLIT="TRAIN" CGISPLIT="TRAINING-SET" OLDID="9917" NEWID="5004">
+<DATE>13-MAR-1987 15:13:01.16</DATE>
+<TOPICS></TOPICS>
+<PLACES><D>usa</D><D>brazil</D></PLACES>
+<PEOPLE></PEOPLE>
+<ORGS></ORGS>
+<EXCHANGES></EXCHANGES>
+<COMPANIES></COMPANIES>
+<UNKNOWN> 
+&#5;&#5;&#5;RM
+&#22;&#22;&#1;f0233&#31;reute
+u f BC-BRAZIL-DEBT-POSES-THO   03-13 0103</UNKNOWN>
+<TEXT>&#2;
+<TITLE>BRAZIL DEBT POSES THORNY ISSUE FOR U.S. BANKS</TITLE>
+<AUTHOR>    By Cal Mankowski, Reuters</AUTHOR>
+<DATELINE>    NEW YORK, March 13 - </DATELINE><BODY>CitiCorp &lt;CCI> appears to be digging
+in its heels for tough negotiations over the billions of
+dollars in loans that Brazil owes to money center banks, Wall
+Street analysts said.
+    "I view it as pre-negotiation posturing," said analyst
+Carole Berger of Cyrus J. Lawrence Inc, referring to both
+Brazil and Citicorp. Brazil recently stopped paying interest on
+its commercial bank debt. Today, CitiCorp said its first
+quarter income would be reduced by 50 mln dlrs after tax if it
+lists the loans as non-performing status.
+    Citicorp filed an 8-K form with the Securities and Exchange
+Commission, indicating a material change in its financial
+situation. "Citicorp is saying you can't scare us with threats,
+we'll make your loans non-performing right now," Berger said.
+    The loans have to be treated as non-performing after 90
+days without a payment. CitiCorp said only that the situation
+will be reviewed at the end of the current quarter.
+    Berger asserted that Brazil is doing its own politicking by
+talking to U.S. regulators and trying to drive a wedge between
+U.S. and European banks.
+    Analyst Lawrence Cohn of Merrill Lynch and Co said it is
+unlikely the situation will be resolved until the second half
+of this year.
+    "Ultimately the Brazilians are going to have to pay up on
+all the interest they owe," Cohn said. "The real issue is the
+rescheduling of debt terms.
+    Another question is whether or not the International
+Monetary Fund can help Brazil with a new austerity program.
+    Stocks of money center banks were mostly down fractions in
+late dealings. One trader most stocks in the group bounced off
+their lows by midday as investors took the news in stride.
+    Cohn said the bank stocks may be risky until numbers on
+non-performing loans are reported for each bank. But he said
+investors looking ahead six to 12 months might want to buy at
+present levels for the "tremendous fundamental values" in the
+group.
+    Analyst Robert Gordon of Shearson Lehman Brothers Inc said
+Manufacturers Hanover Corp &lt;MHC) has the greatest exposure to
+Brazilian loans of any New York bank, in terms of percentage of
+earnings. He said his only two recommendations currently among
+New York banks are J.P. Morgan and Co &lt;JPM> and Bankers Trust
+Co &lt;BT> which happen to have the least exposure.
+    Gordon said his positive opinion on J.P. Morgan and Bankers
+Trust was not merely a response to the fact that the two have
+lower exposure to Brazilian loans than other banks. 
+    In fact he said there's a chance those banks could get more
+involved in the future. He noted that Morgan has already set up
+a brokerage subsidiary to deal in loans to less developed
+countries.
+    "I don't see any reason to change full year earnings
+estimates, said Frederick Meinke, analyst at E.F. Hutton Co. He
+thinks the confrontation with Brazil could end in a replay of a
+situation that occurred with Argentina in 1984 and 1985.
+    Meinke noted that in the case of Argentina the loans became
+non-accruing for a couple of quarters but then when the banks
+came to an agreement with Argentina all the back interest was
+paid. "What it did was distort the quarterly earnings pattern,"
+he said.
+    He said in the case of Brazil write-offs of loans is a
+worst-case outcome which is highly unlikely. He noted that
+Brazil is a country with a diversified economy, going through
+some economic upheaval after the transition from a military to
+a civilian government.
+    "The countries of Latin America have too much debt relative
+to their ability to service it," said John Mason of
+Interstate Securities. "We've been fiddling around with these
+problems for five years and the hour is growing late."
+    He said up to now the banks have reduced their spreads, cut
+interest rates internally and extended maturities and none of
+these measures has been enough. He expects re-classification of
+as much as a third of the loans as non-accruing and he sees
+partial write downs of some loans.
+    Nevertheless Mason thinks the money center bank stocks 
+could be poised for a short term rally.
+    A spokesman at First Chicago Corp &lt;FNB> said it is
+premature to put Brazil's loans on a cash basis. "It is our
+expectation that economic development will allow Brazil to meet
+its debt," a spokesman said.
+    Bankers Trust in New York said it would be premature to
+make a decision. Several other banks queried by Reuters said
+much the same thing.
+    A spokesman at Manufacturers Hanover noted that of 2.3
+billion dlrs in loan exposure to Brazil only 1.3 billion is
+subject to Brazil's unilateral moratorium on repayment of
+interest.
+   
+ Reuter
+&#3;</BODY></TEXT>
+</REUTERS>
+"""
