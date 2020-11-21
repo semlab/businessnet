a0ec5f18493e048bdfd83f66cd8fc8440bd2f791
godidier <godidier@gmail.com>
2020-11-22 01:36:46 +0900

    using proper assert method for preproc tests

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -9,7 +9,7 @@ class ReuterPreprocTestCase1(unittest.TestCase):
     def test_format_articles(self):
         pp = ReuterPreproc(None)
         res = pp.format_articles(REUTERS_CORPUS_SAMPLE)
-        assert(len(res) > 0)
+        self.assertTrue(len(res) > 0)
 
 
 if __name__ == "__main__":
